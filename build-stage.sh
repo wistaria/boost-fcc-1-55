29c2bd6f7e05e181b8edf97c0035fcdcc2bb3331
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-12-23 10:17:10 +0900

    Fixed PREFIX

--- build-stage.sh
+++ build-stage.sh
@@ -5,4 +5,4 @@ SRC_DIR=$(dirname $0)
 
 (cd $SRC_DIR/boost_$BOOST_VERSION && \
  echo "using mpi : $(which mpiFCCpx) ;" > user-config.jam && \
- env BOOST_BUILD_PATH=. $PREFIX_FRONTEND/bin/b2 -d2 --prefix=$PREFIX --layout=tagged --without-context --without-coroutine --without-python toolset=fccx threading=multi variant=release stage) | tee build-stage.log
+ env BOOST_BUILD_PATH=. $PREFIX/bin/b2 -d2 --prefix=$PREFIX --layout=tagged --without-context --without-coroutine --without-python toolset=fccx threading=multi variant=release stage) | tee build-stage.log
