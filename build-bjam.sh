48bbb93db13b745c251dc3ca8c15bd290a86e592
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-12-23 10:30:35 +0900

    Fixed log output

--- build-bjam.sh
+++ build-bjam.sh
@@ -5,4 +5,4 @@ SRC_DIR=$(dirname $0)
 
 (cd $SRC_DIR/boost_$BOOST_VERSION/tools/build/v2 && \
  sh bootstrap.sh && \
- ./b2 --prefix=$PREFIX toolset=gcc install) | tee build-bjam.log
+ ./b2 --prefix=$PREFIX toolset=gcc install) 2>&1 | tee build-bjam.log
--- build-stage.sh
+++ build-stage.sh
@@ -5,4 +5,4 @@ SRC_DIR=$(dirname $0)
 
 (cd $SRC_DIR/boost_$BOOST_VERSION && \
  echo "using mpi : $(which mpiFCCpx) ;" > user-config.jam && \
- env BOOST_BUILD_PATH=. $PREFIX/bin/b2 -d2 --prefix=$PREFIX --layout=tagged --without-context --without-coroutine --without-python toolset=fccx threading=multi variant=release stage) | tee build-stage.log
+ env BOOST_BUILD_PATH=. $PREFIX/bin/b2 -d2 --prefix=$PREFIX --layout=tagged --without-context --without-coroutine --without-python toolset=fccx threading=multi variant=release stage) 2>&1 | tee build-stage.log
